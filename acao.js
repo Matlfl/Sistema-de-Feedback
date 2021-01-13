1f010b45649be964dcd2a2ea1e64a32ca73f0c1f
Mateus Farias <71677164+Matlfl@users.noreply.github.com>
2021-01-13 10:15:17 -0300

    Add files via upload

---
+++ acao.js
@@ -0,0 +1,16 @@
+var click1 = 0
+var click2 = 0
+var click3 = 0
+
+function clique1() {
+  click1++;
+    console.log(click1)
+}
+function clique2() {
+    click2++;
+    console.log(click2)
+  }
+  function clique3() {
+    click3++;
+    console.log("nota3 " + click3)
+  }
